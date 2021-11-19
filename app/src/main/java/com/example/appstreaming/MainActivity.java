1acc5c549722a06b61d123f0a1e2aa3b680f635c
AyrtonGonsallo <ayrtongonsallo444@gmail.com>
2021-11-19 11:57:15 +0100

    debut

--- app/src/main/java/com/example/appstreaming/MainActivity.java
+++ app/src/main/java/com/example/appstreaming/MainActivity.java
@@ -11,3 +11,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
+
+
+
+/*
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/AyrtonGonsallo/Android-Streaming.git
+git push -u origin main
+ */
